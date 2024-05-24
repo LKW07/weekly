fbd796ecadbd415e330e9921d4475f34ce32193c
Leander Weiss <weiss.leander@steinweiss.de>
2024-05-24 10:49:53 +0200

    Update everything

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ let db = new sqlite3.Database("./db/database.db", sqlite3.OPEN_READWRITE, (err) 
     }
 });
 
-let sql = "SELECT username FROM Users";
+let sql = "SELECT username, userID FROM Users";
 
 db.all(sql, [] , (err, rows) => {
   if (err) {
       throw err;
   }
   rows.forEach((row) => {
-    console.log(row.username)
+    console.log(row.userID + " - " + row.username)
   });
 });
 
@@ -29,16 +29,17 @@ app.use(express.urlencoded());
 
 
 // Variablen
-let users = [{username: "lkw", password: "lkw", loggedIn: "false"}];
-let loggedIn = false;
+let users = [{username: "lkw", password: "lkw", loggedInAsUser: "false"}];
+let loggedInAsUser = false;
 let loggedInUser;
+let loggedInAsAdmin;
 
 app.get("/", (req, res) => {
-    if (loggedIn) {
+    if (loggedInAsUser) {
         res.render("home.ejs", {user: loggedInUser || "user"});
     }
     else {
-        res.render("login.ejs");
+        res.redirect("/login");
     }
 });
 
@@ -47,7 +48,7 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-    if (loggedIn) {
+    if (loggedInAsUser) {
         res.render("home.ejs", {user: loggedInUser || "user"});
     }
     else {
@@ -56,7 +57,67 @@ app.get("/home", (req, res) => {
     }
 });
 
+app.get("/podcast", (req, res) => {
+    if (loggedInAsUser) {
+        res.render("podcast.ejs");
+    }
+    else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/podcast-dev", (req, res) => {
+    if (loggedInAsUser) {
+        res.render("podcast-dev.ejs");
+    }
+    else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/admin-panel", (req, res) => {
+    if (loggedInAsAdmin) {
+        res.render("Admin/admin-panel.ejs");
+    }
+    else {
+        res.redirect("/home");
+    }
+});
+
+app.get("*", (req, res) => {
+    res.render("Error/404.ejs");
+});
+
 app.post("/login", (req, res) => {
+    login_user(req, res);
+});
+
+app.post("/logout-user", (req, res) => {
+    loggedInAsUser = false;
+    res.redirect("/login");
+});
+
+app.post("/logout-admin", (req, res) => {
+    loggedInAsAdmin = false;
+    res.redirect("/login");
+});
+
+app.post("/podcast", (req, res) => {
+    res.redirect("/podcast");
+});
+
+app.post("/podcastdev", (req, res) => {
+    res.redirect("/podcast-dev");
+});
+
+app.listen(3000);
+
+
+
+
+// FUNCTIONS
+
+function login_user(req, res) {
     console.log(req.body.username);
     
     const btn = req.body.submit;
@@ -66,36 +127,42 @@ app.post("/login", (req, res) => {
     sql = "SELECT username, password FROM Users WHERE (username = ?) AND (password = ?)";
     params = [username, password];
 
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            throw err;
+    db.all(sql, params, (Uerr, Urows) => {
+        if (Uerr) {
+            throw Uerr;
         }
         else {
-            if (rows.length > 0){
-                loggedInUser = rows[0].username;
-                loggedIn = true;
+            if (Urows.length > 0){
+                loggedInUser = Urows[0].username;
+                loggedInAsUser = true;
                 res.redirect("/home");
             }
-            else {
-                console.log("passsword wrong");
-                res.send("<h1>Wrong Username or Password!</h1><a href='/login'>Back to Login Page</a>");
+            else if (Urows.length == 0) {
+                login_admin(req, res);
             }
         }
     });
+}
 
-    /*if (username == "lkw" && password == "lkw") {
-        res.redirect("/home");
-        loggedIn = true;
-    }
-    else {
-        console.log("passsword wrong");
-        res.send("<h1>Wrong Username or Password!</h1><a href='/login'>Back to Login Page</a>");
-    }*/
-});
+function login_admin(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.post("/logout", (req, res) => {
-    loggedIn = false;
-    res.redirect("/login");
-});
+    sql = "SELECT username, password FROM Admins WHERE (username = ?) AND (password = ?)";
+    params = [username, password];
 
-app.listen(3000);
+    db.all(sql, params, (Aerr, Arows) => {
+        if (Aerr) {
+            throw Aerr;
+        }
+        else  if (Arows.length > 0) {
+            loggedInAsAdmin = true;
+            loggedInUser = Arows[0];
+            res.redirect("/admin-panel");
+        }
+        else {
+            console.log("passsword wrong");
+            res.send("<h1>Wrong Username or Password!</h1><a href='/login'>Back to Login Page</a>");
+        }
+    });
+}
